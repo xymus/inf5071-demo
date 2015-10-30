5131b34096c380f62455251abff3a48e2c24fd2a
Alexis Laferrière <alexis.laf@xymus.net>
2015-10-30 15:27:32 -0400

    demo7: corrige le nombre de colonnes et de lignes
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

--- demo7/shader-checker.glsl
+++ demo7/shader-checker.glsl
@@ -1,12 +1,12 @@
-float n_row = 4.0;
-float n_col = 5.0;
+float n_row = 8.0;
+float n_col = 10.0;
 
 vec4 black = vec4(0.0, 0.0, 0.0, 1.0);
 vec4 cell_color = vec4(0.4, 0.6, 0.4, 1.0);
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    vec2 cell_dim = (iResolution / vec3(n_col, n_row, 0)).xy;
+    vec2 cell_dim = (iResolution / vec3(n_col/2.0, n_row/2.0, 0)).xy;
     vec2 pos_in_cell = mod(fragCoord, cell_dim);
     
     fragColor = cell_color;
