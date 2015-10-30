08c0877854173d31e11edfe8be3d1799a708a3b1
Alexis Laferrière <alexis.laf@xymus.net>
2015-10-30 15:11:16 -0400

    demo7: corrige le check pour remplacer la grille par un échequier
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

--- demo7/shader-checker.glsl
+++ demo7/shader-checker.glsl
@@ -1,8 +1,7 @@
-float n_row = 8.0;
-float n_col = 10.0;
+float n_row = 4.0;
+float n_col = 5.0;
 
-float line_width = 5.0;
-vec4 line_color = vec4(0.0, 0.0, 0.0, 1.0);
+vec4 black = vec4(0.0, 0.0, 0.0, 1.0);
 vec4 cell_color = vec4(0.4, 0.6, 0.4, 1.0);
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
@@ -11,9 +10,10 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 pos_in_cell = mod(fragCoord, cell_dim);
     
     fragColor = cell_color;
-    if (pos_in_cell.x < line_width) {
-        fragColor = line_color;
-    } else if (pos_in_cell.y < line_width) {
-        fragColor = line_color;
+    if (pos_in_cell.x < cell_dim.x/2.0 && pos_in_cell.y < cell_dim.y/2.0) {
+    } else if (pos_in_cell.x < cell_dim.x/2.0) {
+        fragColor = black;
+    } else if (pos_in_cell.y < cell_dim.y/2.0) {
+        fragColor = black;
     }
 }
